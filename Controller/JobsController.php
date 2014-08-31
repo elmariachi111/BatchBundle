b55b24566442fe544418c47f1d3bf10d14d658f1
Stefan <stadolf@gmail.com>
2014-08-31 18:24:33 +0200

    empty bundle

---
+++ Controller/JobsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace  Elmariachi111\Bundle\BatchBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class JobsController extends Controller
+{
+    public function indexAction()
+    {
+        return "Hello";
+    }
+}
---
+++ Elmariachi111BatchBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Elmariachi111\BatchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Elmariachi111BatchBundle extends Bundle
+{
+}
